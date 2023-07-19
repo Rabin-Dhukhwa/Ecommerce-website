f6706aec99263b63e5b7d54085ec91e8e5b11a29
Rabin Dhukhwa <dhukhwarabin@gmail.com>
2023-07-20 09:16:26 +1000

    home add

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -17,39 +17,6 @@ const Home = () => {
     <>
       <Header />
       <main className="main-home">
-        {/* <div className="home-page">
-          <div className="hero animate__animated animate__fadeIn">
-            <h1 className="animate__animated animate__bounceInDown">
-              Welcome to Your CMS
-            </h1>
-            <p className="animate__animated animate__fadeIn">
-              Manage your content efficiently with our powerful CMS
-            </p>
-          </div>
-          <div className="features">
-            <div className="feature animate__animated animate__fadeInLeft">
-              <h2>Create and Edit Content</h2>
-              <p>
-                Easily create and edit various types of content using our
-                intuitive interface.
-              </p>
-            </div>
-            <div className="feature animate__animated animate__fadeInUp">
-              <h2>Manage Media Files</h2>
-              <p>
-                Organize and access your media files in a centralized media
-                library.
-              </p>
-            </div>
-            <div className="feature animate__animated animate__fadeInRight">
-              <h2>User Management</h2>
-              <p>
-                Create and manage user accounts with different roles and
-                permissions.
-              </p>
-            </div>
-          </div>
-        </div> */}
         <div className="home-carousel">
           <CarouselSlider images={images} />
         </div>
